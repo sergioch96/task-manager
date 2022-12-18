23e506b648b0438d0d59dd3426b774054e888147
Sergio Chamorro <sergiochamo96@gmail.com>
2022-12-18 17:54:09 -0300

    Added to create and edit task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,58 @@
 class TasksController < ApplicationController
   
   def index
-    @page = params[:page].to_i
-    @tasks = ['Fold laundry', 'Sweep porch', 'Wash dishes', 'Mow lawn']
+    @tasks = Task.order(:position)
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @count = Task.count
+    @task = Task.new(position: @count + 1)
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      # The 'new' action is NOT being called here
+      # Assign any instance variables needed
+      # @count = Task.count
+      render('new')
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render('edit')
+    end
   end
 
   def delete
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :position,
+      :completed,
+      :description
+    )
   end
 
 end
